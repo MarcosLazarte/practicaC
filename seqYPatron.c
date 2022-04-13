1c0075260f51baa59c7a021e55fcbc0f85784cff
Aky <marcos.e.lazarte@gmail.com>
2022-04-13 18:32:53 -0300

    Se cambio la descripcion del programa seq&patron

--- seqYPatron.c
+++ seqYPatron.c
@@ -1,6 +1,9 @@
 /*
-Programa que devuelve los indexes donde se encuentra un patron en una secuencia, ambos ingresados por el usuario.
-Con motivo
+-Motivo: Hice estos ejercicios con el fin de enteder el funcionamiento del problema que se me habia dado para entrar a una empresa.
+
+-Que hace?: El programa permite el ingreso de dos cadenas de caracteres, para luego encuentra una secuencia de caracteres dentro de una secuencia mayor. Al finalizar, devueve el indice donde se encontro el inicio del patron dentro de la secuencia mayor.
+
+-Como lo hace? Se ingresa el tamanio que tendra el patron dado para luego ingresarlo, de igual manera se ingresa la secuencia donde se buscara el patron. Se apoya en memoria dinamica y comparacion de caracteres con iteraciones de "for"
 */
 #include<stdio.h>
 #include<stdlib.h>
